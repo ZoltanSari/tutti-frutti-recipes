8c8343126065c1b502388a47ba944ebcf1ef68a6
Grob Oliver <groboliver1117@gmail.com>
2019-01-08 10:03:26 +0100

    Backend fix

--- backend/src/main/java/com/codecool/tuttifrutti/service/UserService.java
+++ backend/src/main/java/com/codecool/tuttifrutti/service/UserService.java
@@ -9,7 +9,6 @@ import com.codecool.tuttifrutti.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
