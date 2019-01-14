8c74beb77422687ab3a25de197c5a8b21d22b7f1
ZoltanSari <Kingshapy@gmail.com>
2019-01-14 14:23:57 +0100

    install the bootstrap

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../services/user.service";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {ActivatedRoute} from "@angular/router";
-import {RecipeService} from "../../services/recipe.service";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -15,26 +14,48 @@ export class LoginComponent implements OnInit {
 
   constructor(private userService: UserService,
               private route: ActivatedRoute,
-              private recipeService: RecipeService) { }
+              private router: Router) {}
 
   ngOnInit() {
-    this.recipeService.message
-      .subscribe( message => {
-        console.log('login component: ' + message)
-      }
-  );
     this.login = this.route.routeConfig.path === 'login';
     this.initForm();
   }
 
   onSubmit() {
-    this.userService;
+    if (!this.login) {
+      const date = new Date();
+      const month = date.getMonth() < 10 ? '0' + parseInt(date.getMonth().toString() + 1) : date.getMonth() + 1;
+      const userDetails = {
+        'username': this.loginForm.value['username'],
+        'password': this.loginForm.value['password'],
+        'registrationDate': date.getFullYear() + '-' + month + '-' + date.getDate(),
+        'recipes': [],
+        'likedRecipes': []
+      };
+      this.userService.addUser(userDetails);
+    } else {
+      if (this.isLogin()) {
+        alert('Register please');
+        this.router.navigate(['registration'])
+      } else {
+        this.userService.getUser(this.loginForm.value['username'], this.loginForm.value['password'])
+          .subscribe(
+            user => this.userService.user = user
+          )
+      }
+    }
   }
 
   private initForm() {
     this.loginForm = new FormGroup({
       'username': new FormControl(null, Validators.required),
-      'password': new FormControl(null, Validators.required)
+      'password': new FormControl(null, Validators.required),
     })
+  }
+
+  private isLogin(){
+    const username = this.loginForm.value['username'];
+    const  password = this.loginForm.value['password'];
+    return this.userService.getUser(username, password).subscribe() == null;
   }
 }
