0050c7603450dea882c471843dc9b984d8c30382
ZoltanSari <Kingshapy@gmail.com>
2019-01-24 10:55:49 +0100

    create category enum list to the recipe. We can add categories as well

---
+++ backend/src/main/java/com/codecool/tuttifrutti/model/Category.java
@@ -0,0 +1,17 @@
+package com.codecool.tuttifrutti.model;
+
+public enum Category {
+    FISH,
+    CHICKEN,
+    BEEF,
+    PORK,
+    BREAKFAST,
+    LUNCH,
+    DINNER,
+    SWEETS,
+    VEGETARIAN,
+    PASTA,
+    VEGETABLE,
+    VEGAN,
+    PRESCETARIAN
+}
